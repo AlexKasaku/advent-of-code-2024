31fee5017dcb873a407767170ae30297a0483f21
Alex Washtell <alex@kasaku.co.uk>
2024-12-01 05:14:22 +0000

    2024 - Day 1 - Part 2

--- src/day-01/index.ts
+++ src/day-01/index.ts
@@ -4,7 +4,7 @@ const debugMode = true;
 const debug = (...params: any[]) => debugMode && console.log(...params);
 const log = (...params: any[]) => console.log(...params);
 
-const input = await readInput('day-01')
+const input = await readInput('day-01');
 
 export const part1 = () => {
   const lines = parseLines(input)
@@ -18,19 +18,13 @@ export const part1 = () => {
     list2.push(parseInt(split[1]));
   });
 
-  debug(list1[0]);
-  debug(list2[0]);
-
   list1.sort();
   list2.sort();
 
   let totalDistance = 0;
 
   for (let index = 0; index < list1.length; index++) {
-    debug(list1[index]);
-    debug(list2[index]);
     const diff = Math.abs(list2[index] - list1[index]);
-    debug(diff);
     totalDistance += diff;
   }
 
@@ -39,6 +33,27 @@ export const part1 = () => {
 
 export const part2 = () => {
   const lines = parseLines(input)
-  // your code goes here
-  return lines.length
+  
+  const list1: number[] = [];
+  const rightMap: Map<number, number> = new Map();
+  
+  lines.forEach(line => {
+    const split = line.split('   ');
+    list1.push(parseInt(split[0]));
+    
+    const rightNum = parseInt(split[1]);
+    if (rightMap.has(rightNum))
+      rightMap.set(rightNum, rightMap.get(rightNum)! + 1);
+    else
+      rightMap.set(rightNum, 1)
+  });
+
+  let totalSimilarity = 0;
+
+  for (let index = 0; index < list1.length; index++) {
+    const similar = list1[index] * (rightMap.get(list1[index]) ?? 0);
+    totalSimilarity += similar;
+  }
+
+  return totalSimilarity;
 }
