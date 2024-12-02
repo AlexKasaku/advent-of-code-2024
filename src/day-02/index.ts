295ab35298465181c774a45ab6ead74c9f80be75
Alex Washtell <alex@kasaku.co.uk>
2024-12-02 05:32:58 +0000

    2024 - Day 2 - Part 2

--- src/day-02/index.ts
+++ src/day-02/index.ts
@@ -32,8 +32,37 @@ export const part1 = () => {
   return safeLines
 }
 
+const findUnsafeIndex = (report: number[]) => {
+  const increasing = (report[0] - report[1]) < 0
+
+  for (let x = 1; x < report.length; x++) {
+    const delta = report[x] - report[x - 1]
+    const diff = Math.abs(delta)
+
+    if (diff < 1 || diff > 3) { return x - 1 }
+    if (increasing && delta < 0 || !increasing && delta > 0) { return x - 1 }
+  }
+
+  return -1
+}
+
 export const part2 = () => {
-  const lines = parseInput()
-  // your code goes here
-  return lines.length
+  const numbers = parseInput()
+
+  const safeLines = numbers.filter((report) => {
+    let unsafeIndex = findUnsafeIndex(report)
+    if (unsafeIndex === -1) { return true }
+
+    for (let index = 0; index < report.length; index++) {
+      const updatedReport = [...report]
+      updatedReport.splice(index, 1)
+
+      unsafeIndex = findUnsafeIndex(updatedReport)
+      if (unsafeIndex === -1) { return true }
+    }
+
+    return false
+  }).length
+
+  return safeLines
 }
