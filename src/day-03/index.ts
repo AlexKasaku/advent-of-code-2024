33abcdd61b821a04a079f8a8ebe396a7f679e453
Alex Washtell <alex@kasaku.co.uk>
2024-12-03 23:13:08 +0000

    2024 - Day 3 - Further tidy up

--- src/day-03/index.ts
+++ src/day-03/index.ts
@@ -14,22 +14,18 @@ type Instruction = {
 const parseInput = (): Instruction[] => {
   const text = parseLines(input).join()
 
-  const matches = text.match(/(mul\(\d{1,3},\d{1,3}\))|(do\(\))|(don't\(\))/g) ?? []
+  const regex = /(?<type>(mul|do|don't))\(((?<val1>\d{1,3}),(?<val2>\d{1,3}))?\)/g
+  const matches = [...text.matchAll(regex)]
 
   return matches.map((x) => {
-    if (x === 'do()') { return { type: 'do' } }
-    if (x === 'don\'t()') { return { type: 'dont' } }
+    if (x.groups!['type'] == 'do') { return { type: 'do' } }
+    if (x.groups!['type'] == 'don\'t') { return { type: 'dont' } }
 
-    const m = x.match(/(\d+),(\d+)/)!
-    return { type: 'mul', val1: Number.parseInt(m[1]), val2: Number.parseInt(m[2]) }
+    return { type: 'mul', val1: Number.parseInt(x.groups!['val1']), val2: Number.parseInt(x.groups!['val2']) }
   })
 }
 
-export const part1 = () => {
-  const instructions = parseInput()
-
-  return instructions.filter((i) => i.type === 'mul').map((x) => x.val1 * x.val2).reduce(toSum)
-}
+export const part1 = () => parseInput().filter((i) => i.type === 'mul').map((x) => x.val1 * x.val2).reduce(toSum)
 
 export const part2 = () => {
   const instructions = parseInput()
