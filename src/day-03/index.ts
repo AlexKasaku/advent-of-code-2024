423d2f7d4d9e150eecc33c41e3193eb3587d6479
Alex Washtell <alex@kasaku.co.uk>
2024-12-03 05:23:26 +0000

    2024 - Day 3 - Part 2

--- src/day-03/index.ts
+++ src/day-03/index.ts
@@ -4,25 +4,45 @@ import toSum from 'utils/toSum'
 
 const input = await readInput('day-03')
 
-const parseInput = () => {
+type InstructionType = 'mul' | 'do' | 'dont'
+type Instruction = {
+  type: InstructionType
+  val1?: number
+  val2?: number
+}
+
+const parseInput = (mulOnly: boolean): Instruction[] => {
   const text = parseLines(input).join()
-  const matches = text.match(/mul\(\d{1,3},\d{1,3}\)/g) ?? []
+
+  const matches = (mulOnly
+    ? text.match(/mul\(\d{1,3},\d{1,3}\)/g)
+    : text.match(/(mul\(\d{1,3},\d{1,3}\))|(do\(\))|(don't\(\))/g)
+  ) ?? []
 
   return matches.map((x) => {
+    if (x === 'do()') { return { type: 'do' } }
+    if (x === 'don\'t()') { return { type: 'dont' } }
+
     const m = x.match(/(\d+),(\d+)/)!
-    return { x: Number.parseInt(m[1]), y: Number.parseInt(m[2]) }
+    return { type: 'mul', val1: Number.parseInt(m[1]), val2: Number.parseInt(m[2]) }
   })
 }
 
 export const part1 = () => {
-  const instructions = parseInput()
-  debug(instructions)
+  const instructions = parseInput(true)
 
-  return instructions.map((x) => x.x * x.y).reduce(toSum)
+  return instructions.map((x) => x.val1! * x.val2!).reduce(toSum)
 }
 
 export const part2 = () => {
-  const lines = parseInput()
-  // your code goes here
-  return lines.length
+  const instructions = parseInput(false)
+
+  let enabled = true
+  let total = 0
+
+  instructions.forEach((instruction) => {
+    if (instruction.type === 'mul' && enabled) { total += instruction.val1! * instruction.val2! } else if (instruction.type === 'do') { enabled = true } else if (instruction.type === 'dont') { enabled = false }
+  })
+
+  return total
 }
