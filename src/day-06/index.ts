9b90586e2e79f78d0ab02317ba9e0405a0623ed8
Alex Washtell <alex@kasaku.co.uk>
2024-12-06 06:24:51 +0000

    2024 - Day 6 - Part 2. In need of improvement!

--- src/day-06/index.ts
+++ src/day-06/index.ts
@@ -8,15 +8,19 @@ type Space = Position & {
   isBlocked: boolean
   visited: boolean
 }
+type Guard = Position & { facing: CardinalDirection };
 
-const parseInput = (): { grid: Grid<Space>, guard: Position, facing: CardinalDirection } => {
+const parseInput = (): { grid: Grid<Space>, guard: Guard } => {
   const lines = parseLines(input);
   const values = lines.map((line) => line.split(''));
 
-  let guard: Position;
+  let guard: Guard = { x: 0, y: 0, facing: 'N'};
   const grid = new Grid<Space>(values.length, values[0].length, ({ x, y }) => {
     if (values[y][x] === '^')
-      guard = {x,y};
+    {
+      guard.x = x;
+      guard.y = y;
+    }
 
     return {
       x,
@@ -25,55 +29,164 @@ const parseInput = (): { grid: Grid<Space>, guard: Position, facing: CardinalDir
       visited: values[y][x] === '^'
     }
   });
-  const facing: CardinalDirection = 'N'
 
-  return {grid, guard: guard!, facing};
+  return {grid, guard};
 }
 
-const getSpaceChar = ({isBlocked, visited}: Space, isGuard: boolean) => {
+const getSpaceChar = ({x, y, isBlocked, visited}: Space, isGuard: boolean, candidate?: Space) => {
+  if (candidate?.x === x && candidate?.y === y) return '🟥';
   if (isBlocked) return '🧱';
   if (isGuard) return '🧑';
   if (visited) return '🟨';
   return '⬛';
 };
 
-const renderGrid = (grid: Grid<Space>, guard: Position): void => {
+const renderGrid = (grid: Grid<Space>, guard: Guard, candidate?: Space): void => {
   for (const row of grid.Values)
-    console.log(row.reduce((a, b) => a + getSpaceChar(b, b.x == guard.x && b.y == guard.y), ''));
-  console.log();
+    debug(row.reduce((a, b) => a + getSpaceChar(b, b.x == guard.x && b.y == guard.y, candidate), ''));
+  debug();
 };
 
 export const part1 = () => {
-  let { grid, guard, facing } = parseInput()
-
-  renderGrid(grid, guard);
+  let { grid, guard } = parseInput()
 
   let hasLeftGrid = false;
 
   while (!hasLeftGrid) {
 
-    const spacesToMove = grid.getAllInDirection(guard, facing);
+    const spacesToMove = grid.getAllInDirection(guard, guard.facing);
     let turned = false;
     for(const space of spacesToMove) {
       if (space?.isBlocked) {
-        facing = turnRight(facing);
+        guard.facing = turnRight(guard.facing);
         turned = true;
         break;
       }
       space!.visited = true;
-      guard = {x: space!.x, y: space!.y}
+      guard.x = space!.x;
+      guard.y = space!.y;
     }
     if (!turned)
       hasLeftGrid = true;
   }
   
-  renderGrid(grid, guard);
-
   return grid.Values.flat().filter(x => x.visited).length;
 }
 
 export const part2 = () => {
-  const { grid, guard, facing } = parseInput()
+  let { grid, guard } = parseInput()  
+  const initialGuardPosition: Guard = {...guard};
 
-  return 0
+  let hasLeftGrid = false;
+
+  // Run grid once to get all candidates for blocking
+  while (!hasLeftGrid) {
+
+    const spacesToMove = grid.getAllInDirection(guard, guard.facing);
+    let turned = false;
+    for(const space of spacesToMove) {      
+      if (space?.isBlocked) {
+        guard.facing = turnRight(guard.facing);
+        turned = true;
+        break;
+      }      
+      space!.visited = true;
+      guard.x = space!.x;
+      guard.y = space!.y;
+    }
+    if (!turned)
+      hasLeftGrid = true;
+  }
+  
+  renderGrid( grid, guard );
+
+  // Find candidates for blocking using all visited spaces, except where the guard started
+  //const blockingCandidates = grid.Values.flat().filter(x => x.visited && x.x != initialGuardPosition.x && x.y != initialGuardPosition.y);
+
+  // Find candidates using all unblocked spaces! This can surely be reduced down to spaces visited or next to previous visited spaces
+  let blockingCandidates: Set<Space> = new Set<Space>();
+  grid.Values.flat().forEach(space => {
+    if (!space.isBlocked && space.visited) {
+
+      if (!blockingCandidates.has(space) && !(space.x == initialGuardPosition.x && space.y == initialGuardPosition.y))
+        blockingCandidates.add(space);
+
+      const adjacent = grid.getNeighbours(space, true);
+      adjacent.forEach(adjacentSpace => {
+        if (!adjacentSpace.isBlocked && !blockingCandidates.has(adjacentSpace) && !(space.x == initialGuardPosition.x && space.y == initialGuardPosition.y))
+          blockingCandidates.add(adjacentSpace);
+      })
+    }
+  });
+  //debug(blockingCandidates);
+
+  // Try out blocking candidates and see if it causes a loop
+  let causesLoop: Space[] = [];
+
+  // We'll determine a loop by recording each space and facing. If guard is ever back in a space already visited facing same direction, it'll be a loop
+  const guardStateToString = ({x,y,facing}: Guard) => `${x},${y},${facing}`;
+
+  blockingCandidates.forEach( candidate => {
+
+    // Reset visit state and guard state
+    grid.Values.flat().forEach(space => space.visited = false);
+    guard = {...initialGuardPosition};
+    hasLeftGrid = false;
+
+    // Set initial states
+    const wasPreviousState = ( guard: Guard ) => guardStates.has( guardStateToString(guard) );
+    const addState = ( guard: Guard ) => guardStates.add( guardStateToString(guard) );
+    const guardStates = new Set<string>([...guardStateToString(guard)]);
+
+    // Add block
+    candidate.isBlocked = true;
+
+    // Run grid to see if we loop
+    while (!hasLeftGrid) {
+      const spacesToMove = grid.getAllInDirection(guard, guard.facing);
+      let turned = false;
+      for(const space of spacesToMove) {
+        // Have we been here before?
+        if (wasPreviousState(guard)) {
+
+          //renderGrid(grid, guard, candidate);
+          //debug(`Space x = ${candidate.x}, y = ${candidate.y} will cause a loop when guard is at x = ${guard.x}, y = ${guard.y}`);
+          causesLoop.push(candidate);
+
+          // Unblock candidate space
+          candidate.isBlocked = false;
+
+          // Break right out of candidate test
+          return;
+        };
+
+        // Haven't been here before, record it.
+        addState(guard);
+
+        // Carry on moving
+        if (space?.isBlocked) {
+          guard.facing = turnRight(guard.facing);
+          turned = true;
+          break;
+        }
+        space!.visited = true;
+        guard.x = space!.x;
+        guard.y = space!.y;
+      }
+
+      if (!turned)
+        hasLeftGrid = true;
+    }
+
+    //renderGrid(grid, guard, candidate);
+
+    // Unblock candidate space
+    candidate.isBlocked = false;
+
+    // Left grid, so let's move on to next candidate
+  })
+
+  debug(causesLoop);
+
+  return causesLoop.length;
 }
