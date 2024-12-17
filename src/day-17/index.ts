ba8e4c2157c705eb054dbb08b5a48058d1c90022
Alex Washtell <alex@kasaku.co.uk>
2024-12-17 21:03:29 +0000

    2024 - Day 17 - Part 2

--- src/day-17/index.ts
+++ src/day-17/index.ts
@@ -1,6 +1,7 @@
 import chalk from 'chalk'
-import { parseLines, readInput } from 'io'
-import { log, debug } from 'log'
+import { readInput } from 'io'
+import { debug } from 'log'
+import { asc } from 'utils'
 
 const input = await readInput('day-17')
 
@@ -47,7 +48,7 @@ const executeOperation = (opcode: number, operand: number, state: State) => {
       break;
     case 1:
       // BXL
-      registers.b = registers.b ^ operand;
+      registers.b = (registers.b ^ operand) >>> 0;
       break;
     case 2:
       // BST
@@ -62,7 +63,7 @@ const executeOperation = (opcode: number, operand: number, state: State) => {
       break;
     case 4:
       // BXC
-      registers.b = registers.b ^ registers.c;
+      registers.b = (registers.b ^ registers.c) >>> 0;
       break;
     case 5:
       // OUT
@@ -116,9 +117,72 @@ export const part1 = () => {
   return state.output.map(x => x.toString()).join(',');
 }
 
+export const getOutputForRegisterA = (a: number, program: number[]): number[] => {
+  const state: State = {
+    registers: { a, b: 0, c: 0 },
+    pointer: 0,
+    output: []
+  }
+
+  while (state.pointer < program.length) {
+    executeOperation(program[state.pointer], program[state.pointer + 1], state);
+  }
+
+  return state.output;
+}
+
+// This is the equivalent of the program but simplified down without the need to pass in the program. 
+// It will generate the same answer.
+export const getOutputFromQuickProgram = (aIn: number): number[] => {
+
+  let a = aIn, b = 0, c = 0;
+  const output: number[] = [];
+
+  while (a > 0) {
+    b = (a % 8) ^ 3;
+    b = ((b ^ Math.trunc(a / Math.pow(2, b))) ^ 5) >>> 0;
+    a = Math.trunc(a / 8)
+    //debug(`A: ${a} B: ${b} Output: ${b % 8}`);
+    output.push(b % 8);
+  }
+
+  return output;
+}
+
 export const part2 = () => {
-  const data = parseInput()
-  return 0;
+
+  const digits = [0, 1, 2, 3, 4, 5, 6, 7].map(x => x.toString())
+  const prefixes = [""];
+  const answers = [];
+
+  const target = [2, 4, 1, 3, 7, 5, 4, 2, 0, 3, 1, 5, 5, 5, 3, 0];
+
+  while (prefixes.length > 0) {
+    const prefix = prefixes.pop()!;
+
+    for (const digit of digits) {
+      const testNumberOct = (prefix + digit).padEnd(16, "0");
+      const testNumberInt = parseInt(testNumberOct, 8);
+
+      const output = getOutputFromQuickProgram(testNumberInt);
+
+      // Depending on the size of the prefix we want to test the digit at the right location (all the other ones
+      // after it should match based on previous prefixes)
+      const position = 15 - prefix.length;
+      if (output[position] == target[position]) {
+        if (position == 0) {
+          debug(testNumberOct);
+          debug(testNumberInt);
+          answers.push(testNumberInt);
+        }
+        else {
+          prefixes.push(prefix + digit);
+        }
+      }
+    }
+  }
+
+  return answers.sort(asc)[0];
 }
 
 
