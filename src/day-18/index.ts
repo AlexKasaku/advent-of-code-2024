580ff87c7cb22379c293723a73745715c68b9711
Alex Washtell <alex@kasaku.co.uk>
2024-12-18 06:30:50 +0000

    2024 - Day 18 - Part 2 - Bit slow!

--- src/day-18/index.ts
+++ src/day-18/index.ts
@@ -37,23 +37,67 @@ type Route = {
 
 const getStateKey = ({ position: { x, y } }: State): string => `${x},${y}`;
 
+// Example setings
+// const first = 12;
+// const width = 7;
+// const height = 7;
+// const start = { x: 0, y: 0 };
+// const end = { x: 6, y: 6 };
+
+// Input setings
+const first = 1024;
+const width = 71;
+const height = 71;
+const start = { x: 0, y: 0 };
+const end = { x: 70, y: 70 };
+
+const canReachEnd = (grid: Grid<Space>, start: { x: number; y: number; }, end: { x: number; y: number; }) => {
+  const lowestCostForState = new Map<string, number>();
+  const queue = priorityQueue<Route>((a, b) => a.steps <= b.steps);
+
+  queue.insert(
+    {
+      state: { position: start },
+      positions: [],
+      steps: 0,
+    }
+  );
+
+  while (!queue.isEmpty()) {
+    const { state, positions, steps } = queue.dequeue()!;
+    const { position } = state;
+
+    if (position.x === end.x && position.y === end.y) {
+      return true;
+    }
+
+    // Have we been to this space before, and was it with a lower value?
+    const stateKey = getStateKey(state);
+    const previousVisit = lowestCostForState.get(stateKey);
+    if (previousVisit !== undefined && previousVisit <= steps) continue;
+
+    // This is new or better visit for this space
+    lowestCostForState.set(stateKey, steps);
+
+    // Add movements for all directions, as long as we haven't visited them or they're blocked.
+    const visitedPositions = new Set<Position>(positions);
+    const unblockedNeighbours = grid.getNeighbours(position, true).filter(s => !s.isBlocked && !visitedPositions.has(s));
+
+    for (const neighbour of unblockedNeighbours) {
+      queue.insert({
+        state: {
+          position: neighbour,
+        },
+        positions: [...visitedPositions, neighbour],
+        steps: steps + 1,
+      });
+    }
+  }
+  return false;
+}
+
 export const part1 = () => {
   const positions = parseInput()
-
-  // Example setings
-  // const first = 12;
-  // const width = 7;
-  // const height = 7;
-  // const start = { x: 0, y: 0 };
-  // const end = { x: 6, y: 6 };
-
-  // Input setings
-  const first = 1024;
-  const width = 71;
-  const height = 71;
-  const start = { x: 0, y: 0 };
-  const end = { x: 70, y: 70 };
-
   const grid = buildGrid(positions.slice(0, first), width, height);
 
   const lowestCostForState = new Map<string, number>();
@@ -104,6 +148,22 @@ export const part1 = () => {
 }
 
 export const part2 = () => {
-  const lines = parseInput()
-  return 0;
+  const positions = parseInput()
+  const grid = buildGrid(positions.slice(0, first), width, height);
+
+  const remainingBytes = positions.slice(first);
+
+  for (const byte of remainingBytes) {
+
+    // Block off the next byte
+    debug(byte);
+    grid.get(byte)!.isBlocked = true;
+
+    // Try and reach end
+    if (!canReachEnd(grid, start, end))
+      return `${byte.x},${byte.y}`;
+
+  }
+
+  throw 'Unable to find byte that blocks'
 }
