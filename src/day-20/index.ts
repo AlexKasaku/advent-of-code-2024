1224b9b0b68f53b8cb5ee3686b73efca313f1008
Alex Washtell <alex@kasaku.co.uk>
2024-12-20 07:41:37 +0000

    2024 - Day 20 - Part 1

--- src/day-20/index.ts
+++ src/day-20/index.ts
@@ -1,6 +1,7 @@
 import { parseLines, readInput } from 'io'
 import { log, debug } from 'log'
 import { Grid, Position } from 'utils/grid'
+import toSum from 'utils/toSum';
 
 const input = await readInput('day-20')
 
@@ -30,15 +31,17 @@ const parseInput = (): { grid: Grid<Space>, start: Position, end: Position } => 
   return { grid, start: start!, end: end! };
 }
 
-const updateGridSteps = (grid: Grid<Space>, start: Position, end: Position): void => {
+const updateGridSteps = (grid: Grid<Space>, start: Position, end: Position): Set<Space> => {
 
   // Walk to end, increasing step counter as we go. Can assume there's only one path to take each time as
   // this isn't a maze with different routes.
   let currentPosition = grid.get(start)!;
+  let stepsOnRoute = new Set<Space>();
 
   let stepCounter = 0;
   while (!(currentPosition.x == end.x && currentPosition.y == end.y)) {
 
+    stepsOnRoute.add(currentPosition);
     currentPosition.step = stepCounter;
     stepCounter++;
 
@@ -53,18 +56,41 @@ const updateGridSteps = (grid: Grid<Space>, start: Position, end: Position): voi
 
   // Need to update for last position
   currentPosition.step = stepCounter;
+  stepsOnRoute.add(currentPosition);
 
+  return stepsOnRoute;
 }
 
 export const part1 = () => {
   const { grid, start, end } = parseInput()
 
-  updateGridSteps(grid, start, end);
+  // Set steps
+  const stepsOnRoute = updateGridSteps(grid, start, end);
 
-  debug(grid.Values)
-  debug(grid.get(end)!.step)
+  const desiredSaving = 100;
+  const combos = new Map<number, number>();
 
-  return 0
+  // Now find jumps that would increase steps.
+  for (const stepOnRoute of stepsOnRoute) {
+    // Go through each step on the route and see where you could leap to, count it if it
+    // increases the step counter by at least X
+
+    // Reachable spaces are a neighbour of a neighbour, for the first consider walls
+    for (const neighbour of grid.getNeighbours(stepOnRoute, true).filter(s => s.isWall)) {
+      // Now get neighbours spaces that aren't the one we started on
+      const neighbourSpaces = grid.getNeighbours(neighbour).filter(s => !s.isWall && s != stepOnRoute);
+
+      for (const finalSpace of neighbourSpaces) {
+        const saving = (finalSpace.step ?? 0) - (stepOnRoute.step ?? 0) - 2;
+
+        if (saving > 0)
+          combos.set(saving, (combos.get(saving) ?? 0) + 1);
+      }
+    }
+
+  }
+
+  return combos.entries().map(([saving, count]) => saving >= desiredSaving ? count : 0).reduce(toSum)
 }
 
 export const part2 = () => {
